c8362f72e2722483f127f68b0329539ba23df6f5
sumanareddy <sumanareddy.guc27@gmail.com>
2020-04-10 15:18:44 -0500

    initial commit

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+//Set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb+srv://<s538360@nwmissouri.edu>:<Rakesh@11>@cluster0-gd0nc.mongodb.net/local_library?retryWrites=true&w=majority';
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
